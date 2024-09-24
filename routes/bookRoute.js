fd6d76b159bf517ba01f5bcee31470a441f6352f
Santhosh <111000634+TechWriter03@users.noreply.github.com>
2024-09-24 20:32:33 +0530

    Update bookRoute.js

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router()
 const { createBook, getAllBooks, getOneBook, updateBook, deleteBook } = require('../controllers/bookController')
 
